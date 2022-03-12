934f9ca59044f8b115c8696acd6c484b83a0fafb
SitiUlunNuhaKarimah127 <200411100127@student.trunojoyo.ac.id>
2022-03-13 00:23:34 +0700

    first commit

---
+++ variable.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 6;
+$b = 3;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr";
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+// perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>"
+
+?>
