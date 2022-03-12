917a2ec9ca9eef70db9b73eb9e4c55eb3c686959
SitiUlunNuhaKarimah127 <200411100127@student.trunojoyo.ac.id>
2022-03-13 00:20:21 +0700

    first commit

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Nama Dan NIM" ?></title>
+    </head>
+    <body>
+        <?php
+        // menulis nama
+            echo "NAMA : SITI ULUN NUHA KARIMAH <br>";
+        // menulis NIM
+            echo "<p>NIM : 200411100127</P>";
+        ?>
+    </body>
+</html>
